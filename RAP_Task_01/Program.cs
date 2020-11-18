a39aaa003425b0afac6ee1c78b52a80ad4301c1f
Yasuo78 <45259890+Yasuo78@users.noreply.github.com>
2020-11-18 17:06:32 +0300

    Task_01

---
+++ RAP_Task_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RAP_Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldCoins;
+            int priceCrystals = 15;
+            int byuCrystals;
+            bool optionPurchase;
+
+            Console.Write("Сколько у вас золотых монет? - ");
+            goldCoins = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Цена кристалла {priceCrystals} монет. Сколько вы хотите купить кристаллов? - ");
+            byuCrystals = Convert.ToInt32(Console.ReadLine());
+
+            optionPurchase = goldCoins >= priceCrystals * byuCrystals;
+            byuCrystals *= Convert.ToInt32(optionPurchase);
+            goldCoins -= priceCrystals * byuCrystals;
+
+            Console.WriteLine($"У вас в сумке осталось {goldCoins} монет и появилось {byuCrystals} кристаллов.");
+        }
+    }
+}
